91d5a5444248b0e1e9ffb4e4858c0aa05ed77e3a
Axel Maczkowiak <axel.maczkowiak@live.fr>
2023-04-01 12:44:46 +0200

    feat(style): Added & configured Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TeamsService} from "./api/api/teams.service";
+import {Team} from "./api/model/team";
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,16 @@ import {TeamsService} from "./api/api/teams.service";
 export class AppComponent implements OnInit {
   title = '24h-2023';
 
+  fetchStatus: 'loading' | 'success' | 'error' = 'loading';
+  ourTeam!: Team;
+
   constructor(public teamsService: TeamsService) {
   }
 
   ngOnInit(): void {
-    this.teamsService.getAllTeams().subscribe(result => {
-      console.log('Teams : ',result)
+    this.teamsService.getById(11).subscribe(result => {
+      this.fetchStatus = 'success';
+      this.ourTeam = result;
     })
   }
 }
